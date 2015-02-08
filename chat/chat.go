7beb61080f87a8b337f42a79dfad58d4f8c862fa
Arijit Sarcar <sarcar_a@yahoo.com>
2015-02-07 19:25:47 -0800

    Added flavor to be able to chat with a Bot if no pair exists!

--- chat/chat.go
+++ chat/chat.go
@@ -4,10 +4,14 @@ import (
 	"code.google.com/p/go.net/websocket"
 	"flag"
 	"fmt"
+	"github.com/asarcar/go_test/misc"
 	"html/template"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -15,64 +19,127 @@ func main() {
 	setHandleFuncs()
 }
 
+var gChain *misc.Chain
+var gChatTemplate *template.Template
+
+const (
+	kListenAddr = "localhost:4000"
+	// Server allows onto to chat with a virtual bot
+	// max time before one is paired with bot
+	kMaxChatWaitTime = 10
+	// markov chain prefix length used to generate sentences by bot
+	kMaxPrefixLen = 2
+	// max # of words spewed at a time by bot
+	kMaxWordsGen = 10
+	// Default String with which generator is seeded
+	kStr = `By now, most high school seniors planning to attend college 
+       in the fall have selected their chosen institute of higher 
+       education. It’s an exciting time for you, Wildcats '13, 
+       and you probably have some questions about your future. 
+       Such as, who will I meet? What clubs will I join? 
+       What if my roommate only wants to stay in the room 
+       eating cold cuts and watching Moesha re-runs? 
+       Will I decide to buy a body pillow from Bed Bath and 
+       Beyond? (Yes, besides being extremely comfortable body 
+       pillows are an excellent way to block you from other 
+       people's booger walls). In an effort to get to know each 
+       other a little better before the fall rolls around, several 
+       members of Columbia University’s future class of 2017 
+       uploaded their college application essays into a shared 
+       Google doc. That Google doc, which contains 70 essays that 
+       either answer the Columbia essay prompt or the Common app 
+       prompt, was then shared with us. And now with you.`
+)
+
 func setGlobals() {
 	htmlDir := parseFlags()
 	gChatTemplate = getTemplate(htmlDir, "chat.html")
-}
 
-const (
-	listenAddr = "localhost:4000"
-)
+	// Seeds the first set of random text spewed by Bot
+	rand.Seed(time.Now().UnixNano()) // Seed the random number generator.
+	gChain = misc.NewChain(kMaxPrefixLen)
+	gChain.Build(strings.NewReader(kStr))
+}
 
 func setHandleFuncs() {
 	http.HandleFunc("/", rootHandler)
 	http.Handle("/chat", websocket.Handler(socketHandler))
 
 	log.Println("Chat Server Started...")
-	log.Fatal(http.ListenAndServe(listenAddr, nil))
+	log.Fatal(http.ListenAndServe(kListenAddr, nil))
 }
 
-var gChatTemplate *template.Template
-
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	gChatTemplate.Execute(w, listenAddr)
+	gChatTemplate.Execute(w, kListenAddr)
 }
 
 type socket struct {
 	// A pass through for read/write can also be implemented by
 	// simply listing the interface name: io.ReadWriter
 	// io.ReadWriter
-	conn *websocket.Conn
+	io.Reader
+	io.Writer
 	done chan bool
-}
-
-func (s socket) Read(msg []byte) (n int, err error) {
-	return s.conn.Read(msg)
-}
-
-func (s socket) Write(msg []byte) (n int, err error) {
-	return s.conn.Write(msg)
 }
 
 func (s socket) Close() error {
 	s.done <- true
-	s.conn.Close()
 	return nil
 }
 
 // websocket.Conn is held open by its handler function
 // The handler is kept running using the channel receive
 // until an explicit Close is called on the socket
+// When we are simulating chat with a virtual person
+// randomly emitting text
 func socketHandler(ws *websocket.Conn) {
-	s := socket{conn: ws, done: make(chan bool)}
+	r, w := io.Pipe()
+
+	go func() {
+		_, err := io.Copy(io.MultiWriter(w, gChain), ws)
+		w.CloseWithError(err)
+	}()
+
+	s := socket{r, ws, make(chan bool)}
 	go matchChat(s)
+
 	<-s.done
+}
+
+// Bot returns an io.ReadeWriteCloser that responds to
+// each incoming write with a generated sentence
+type bot struct {
+	io.ReadCloser
+	out io.Writer
+}
+
+// Read or Close of Bot delegated to Read/Close(Pipe)
+func Bot() io.ReadWriteCloser {
+	r, out := io.Pipe()
+	return bot{r, out}
+}
+
+// Pipe links the Write(Bot) to the Write(Pipe)
+// WR(bot) throws away any data
+// Also triggers WR(Pipe) with generated data ->
+// Data available on RD end of Pipe ->
+// RD(bot) == RD(Pipe) will get generated data
+func (b bot) Write(buf []byte) (int, error) {
+	go b.speak()
+	return len(buf), nil
+}
+
+func (b bot) speak() {
+	time.Sleep(time.Second)
+	b.out.Write([]byte(gChain.Generate(kMaxWordsGen)))
 }
 
 // Channel where pairing channel received/sent
 var pair = make(chan io.ReadWriteCloser)
 
 func matchChat(c io.ReadWriteCloser) {
+	var p io.ReadWriteCloser
+
 	// Channel where termination of chat is received
 	errc := make(chan error)
 
@@ -82,16 +149,21 @@ func matchChat(c io.ReadWriteCloser) {
 	// If (a) happens first then by definition case (b) is
 	// executed on c and if (b) happens then case (a) on p.
 	select {
-	case p := <-pair:
+	case p = <-pair:
 		log.Println("Chat pair established!")
-		// stich the read and write ends to facilitate the chat
-		go cp(c, p, errc)
-		go cp(p, c, errc)
-		defer c.Close()
-		defer p.Close()
 	case pair <- c:
+		// p's goroutine would handle the chat session: we are done
 		return
+	case <-time.After(kMaxChatWaitTime * time.Second):
+		log.Println("Chat pair simulated via Bot!")
+		p = Bot()
 	}
+
+	// stich the read and write ends to facilitate the chat
+	go cp(c, p, errc)
+	go cp(p, c, errc)
+	defer c.Close()
+	defer p.Close()
 
 	// Wait for this chat session to complete
 	err := <-errc
@@ -103,7 +175,7 @@ func matchChat(c io.ReadWriteCloser) {
 }
 
 func cp(c, p io.ReadWriteCloser, errc chan<- error) { // write only channel
-	fmt.Fprint(c, "Chat Session Established!\n")
+	fmt.Fprint(c, "CHAT Session Established!\n")
 	_, err := io.Copy(c, p)
 	errc <- err
 }
--- misc/compose.go
+++ misc/compose.go
@@ -1,6 +1,20 @@
 package misc
 
+import (
+	"fmt"
+)
+
 type Fn func(int) int
+
+func DumpCompose() {
+	var sqr Fn = func(x int) int { return x * x }
+	var inc Fn = func(x int) int { return x + 1 }
+	var dec Fn = func(x int) int { return x - 1 }
+	fmt.Println("---------------------------------------------")
+	fmt.Printf("inc(sqr(dec(1)))=%d, dec(sqr(inc(1)))=%d\n",
+		Compose(inc, sqr)(dec)(1), Compose(dec, sqr)(inc)(1))
+	fmt.Println("---------------------------------------------")
+}
 
 // Must implement f(x)
 type ComposeFns interface {
